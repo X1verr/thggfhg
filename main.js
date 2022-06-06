c6ab5cd5524b41c4befba775421978effd624da3
X1verr <84106967+X1verr@users.noreply.github.com>
2022-06-06 19:59:32 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+img = "";
+status= "";
+objects= "";
+
+function setup() {
+    canvas= createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML =  "Status : Dectecting Objects"
+}
+
+function preload(){
+    img = loadImage('backgroundddd.jpg')
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420)
+
+    if (status  != "")
+    {
+        for(i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innterHTML = "Status : Object Detected";
+
+
+            fill("#fc0303");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("#fc0303");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+
+    
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
